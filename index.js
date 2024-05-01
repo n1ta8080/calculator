4d60e9dc4cc58988582858301977dd7d8891fcb2
nita8080 <olenaksendz2003@gmail.com>
2024-05-01 10:07:18 +0300

    changes

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const Calculator = (() => {
   let result;
 
+  function parseIfStr(a, b) {
+    let numA = a;
+    let numB = b;
+
+    // Перевіряємо, чи a або b не є undefined
+    if (a === undefined || b === undefined) {
+      return [numA, numB]; // Повертаємо вихідні значення без перетворення
+    }
+
+    if (typeof a === "string") {
+      const aParsed = Number(a);
+      if (isNaN(aParsed)) {
+        console.log("Sorry, but the first value is a string and I cannot convert it to a number");
+      } else {
+        numA = aParsed;
+      }
+    }
+
+    if (typeof b === "string") {
+      const bParsed = Number(b);
+      if (isNaN(bParsed)) {
+        console.log("Sorry, but the second value is a string and I cannot convert it to a number");
+      } else {
+        numB = bParsed;
+      }
+    }
+
+    return [numA, numB];
+  }
+
   function add(a, b) {
-    result = a + b;
+    const [numA, numB] = parseIfStr(a, b);
+    result = numA + numB;
     return result;
   }
 
   function subtract(a, b) {
-    result = a - b;
+    const [numA, numB] = parseIfStr(a, b);
+    result = numA - numB;
     return result;
   }
 
@@ -31,3 +63,6 @@ console.log(Calculator.add(5, 3)); // Output: 8
 console.log(Calculator.getMemoryAsString()); // Output: "8"
 Calculator.clearMemory();
 console.log(Calculator.getMemoryAsString()); // Output: "0"
+console.log(Calculator.add('2', 3)); // Output: 5
+console.log(Calculator.getMemoryAsString()); // Output: "5"
+console.log(Calculator.add(undefined, undefined)); // результат буде NaN
